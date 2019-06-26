88027c0067eff1c0c1abbd6fde7da27e0d615e8a
Alison Ming <adk33@cam.ac.uk>
2019-06-26 15:32:56 +0100

    minor update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README')) as f:
       long_description = f.read()
 
 setup (name="pytropd",
-	version="1.0.5",
+	version="1.0.6",
         description = "Calculation of metrics of tropical width",
 	long_description=long_description,
         license = "GPL-3",
@@ -17,7 +17,9 @@ setup (name="pytropd",
         author_email="admg26@gmail.com",
         url="https://tropd.github.io/pytropd/index.html",
 	requires=['numpy','matplotlib','scipy'],
+        install_requires=['numpy>=1.12.0'],
+        python_requires=">=3.5",
 	packages=["pytropd"],
-        classifiers=["Programming Language :: Python :: 2"],
+        classifiers=["Programming Language :: Python :: 3"],
 )
 
